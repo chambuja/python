3ff8de23609a2da27019853fc3b8c4501548e10c
chambuja <166516103+chambuja@users.noreply.github.com>
2024-04-09 21:42:55 +0900

    Add files via upload

---
+++ day01/input.py
@@ -0,0 +1,13 @@
+#print: 글 나오도록 하는 기능
+#input:글을 입력하는 기능
+#drama = input("당신의 최고 드라마는?")
+#print(f"당신의 쵀애 드라마는 {drama}이군요?")
+#
+#name = input("당신의 이름은?")
+#age = input("당신의 나이는?")
+#color = input("당신의 색깔은?")
+#print(f"당신의 이름 {name} 당신의 나이 {age} 좋아하는 색깔 {color}")
+
+
+
+
---
+++ day01/print.py
@@ -0,0 +1,13 @@
+print("hello world!")
+print("안녕하세요? 저는 백광현입니다. 나이는 58세입니다.")
+#
+#오른쪽 데이터를 욎쪽의 변수에 넣는다.
+name = "백광현"
+major = "화학""사과"
+fruit = "사과"
+#f문자열 문법
+print(f"저는 {name}입니다.")
+#저의 이름은 백광현
+print(f"저는 {name}이고 전공은{major}입니다. 좋아하는 과일은 {fruit}입니다.")
+
+print(name)
