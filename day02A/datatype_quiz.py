d3276f512dc2bedc837c88f56a1ad6b62737ed69
chambuja <166516103+chambuja@users.noreply.github.com>
2024-04-11 21:51:02 +0900

    Add files via upload

---
+++ day02A/datatype_quiz.py
@@ -0,0 +1,35 @@
+#유저에게
+#첫번째 정수입력:
+#두번째 정수입력:
+#받은후 두 수의 합을 출력하는 프로그램
+#ex) 첫번재 정수입력: 5
+#두번째 정수입력 : 3
+# 결과 : 8
+a = input("첫 번째 정수입력:")
+b = input("두 번째 정수입력:")
+result = int(a)+int(b)
+print(f"결과 : {result}")3
+
+# 나이를 통한 출생년도 알아내기
+# 나이가 20살이라면 , 출생년도는 2003년 입니다. "
+age= input("나이는")
+year = input("올해는 몇년이죠?")
+ result = int(year)-int(age)
+ print(f"태어난 해는: {result}")
+# 나이를 통한 출생년도 알아내기
+# 나이가 20살이라면 , 출생년도는 2003년 입니다. "
+age = input("나이는")
+year = input("올해는 몇년이죠?")
+result = int(year) - int(age)
+print(f"태어난 해는: {result}")
+# 정사각형의 넓이 구하기
+#문제) 한 변의 길이를 입력받아, 정사각형의 넓이를 구하는 프로그램을 만들어 보세요
+
+side = input("정사각형의 한변의 길이:")
+width = int(side) * int(side)
+print(f"한변의 길이가 {side}라면, 넓이는 {width}입니다.")
+
+
+
+
+
